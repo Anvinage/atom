373ca01ee26bf81dce6461a9fff633fc207a1239
Anvinage <eaa23avi@students.eaaa.dk>
2024-04-17 10:32:36 +0200

    opdate

--- js/script.js
+++ js/script.js
@@ -1,25 +1,5 @@
 'use strict';
 
-// let scrollContainer = document.querySelector('.slider');
-// let upBtn = document.getElementById('upBtn');
-// let downBtn = document.getElementById('downBtn');
-
-// scrollContainer.addEventListener('wheel', (evt) => {
-//     evt.preventDefault();
-//     scrollContainer.style.scrollBehavior = "auto";
-//     scrollContainer.scrollLeft += evt.deltaY;
-// });
-
-// downBtn.addEventListener('click', () => {
-//     scrollContainer.style.scrollBehavior = "smooth";
-//     scrollContainer.scrollLeft += 900;
-// });
-
-// upBtn.addEventListener('click', () => {
-//     scrollContainer.style.scrollBehavior = "smooth";
-//     scrollContainer.scrollLeft -= 900;
-// });
-
 var currentIndex = 0;
     var slides = document.querySelectorAll('.slide');
     var visibleSlides = 3; // Number of slides visible at a time
